126139b51ba0d34d0c94f6d3bf24b064892650af
Adrian Ross <adriantoddross@gmail.com>
2025-06-23 18:20:42 -0700

    Pseudocode for testing getAllFormValues

--- src/tests/discountCalculatorForm.test.ts
+++ src/tests/discountCalculatorForm.test.ts
@@ -8,11 +8,14 @@ test("Numbers are formatted into dollar amounts", () => {
 /*
   test: Form data is converted into. object with props
   expect that getAllFormValues returns normal object with properties
-*/
 
-/*
-  test: Summary is created accurately
-  expect that object matches object shape:
+  create new formobj
+append properties
+
+formValues obj = {"discount-type":"percent","price":"2332","discount":"23"}
+
+  test: Summary is created accurately with getAllFormValues
+  expect that object matches object shape with toMatchObject
       {
         discount (discount-type): "percent"
         amount (amount of discount): discountAmount
