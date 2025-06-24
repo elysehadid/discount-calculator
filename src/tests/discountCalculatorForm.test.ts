45a48b859deb07b08097724f146bd0b7168e4325
Adrian Ross <adriantoddross@gmail.com>
2025-06-23 17:47:21 -0700

    Stub tests

--- src/tests/discountCalculatorForm.test.ts
+++ src/tests/discountCalculatorForm.test.ts
@@ -4,3 +4,19 @@ import { formatToDollar } from "../utils";
 test("Numbers are formatted into dollar amounts", () => {
   expect(formatToDollar(1337.54)).toBe("$1,337.54");
 });
+
+/*
+  test: Form data is converted into. object with props
+  expect that getAllFormValues returns normal object with properties
+*/
+
+/*
+  test: Summary is created accurately
+  expect that object matches object shape:
+      {
+        discount (discount-type): "percent"
+        amount (amount of discount): discountAmount
+        difference (savings): price - discountAmount
+        price
+      }
+*/
